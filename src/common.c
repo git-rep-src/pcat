6fd1b3c9b2e8ccb3e95b5d3b790e5bd16ef8d610
git-rep-src <git-rep@protonmail.com>
2021-04-24 17:59:36 -0300

    Update help message

--- src/common.c
+++ src/common.c
@@ -13,38 +13,12 @@ void print_message(const char *src)
     "\n"
     "Usage: post <option> <arguments>\n"
     "\n"
-    "  set  os (bsd|linux|windows)                      Set remote operating system.\n"
-    "  show (system|users|process|network|pe|exploits)  Show information.\n"
-    "  run  tty                                         Spawn TTY shell.\n"
-    "  pull (tools|exploits) <dir>                      Download and install package.\n"
-    "  doc  (iptables|ssh|sql|pe)                       Show cheatsheet.\n"
-    "  help                                             Show help.\n"
-    "\n"
-    "  Windows: cmd shell is mandatory.\n";
-
-    if (src != NULL)
-        Write(STDOUT_FILENO, src, strlen(src));
-    else
-        Write(STDOUT_FILENO, help, strlen(help));
-}
-/*
-void print_message(const char *src)
-{
-    const char help[] = 
-    "\n"
-    "Usage: post <option> <arguments>\n"
-    "\n"
-    "  set  <arg> <arg>  set variable\n"
-    "                    os bsd|linux|windows\n"
-    "  show <arg>        show information\n"
-    "                    system|users|process|network|pe|exploits\n"
-    "  run  <arg>        run script\n"
-    "                    tty\n"
-    "  pull <arg> <dir>  install package\n"
-    "                    tools|exploits\n"
-    "  doc  <arg>        print cheatsheet\n"
-    "                    iptables|pe|ssh|sql\n"
-    "  help              show help\n"
+    "  set  os [bsd|linux|windows]                        set remote operating system\n"
+    "  show [system|users|process|network|pe|exploits]    show information\n"
+    "  run  tty                                           spawn tty shell\n"
+    "  pull [tools|exploits] <dir>                        download and install package\n"
+    "  doc  [iptables|ssh|sql|pe]                         show cheatsheet\n"
+    "  help                                               show help\n"
     "\n"
     "  windows: cmd shell is mandatory\n";
 
@@ -53,4 +27,3 @@ void print_message(const char *src)
     else
         Write(STDOUT_FILENO, help, strlen(help));
 }
-*/
