64b8b67da202754a95aae0ac5378909dc243c8cf
git-rep-src <git-rep@protonmail.com>
2021-06-29 17:30:00 -0300

    Delete conv

--- src/conv.h
+++
@@ -1,7 +0,0 @@
-#ifndef CONV_H
-#define CONV_H
-
-int utf8_to_unicode(unsigned char *utf8_str, unsigned short *unicode_str, int unicode_str_size);
-void unicode_to_utf8(unsigned int *unistr, long slen, char *utf8_str, long utf8_str_size);
-
-#endif
